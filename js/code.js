9d19109cf285ae821594e5b6d59a7e0162bf331e
ethanosorio <101898908+ethanosorio@users.noreply.github.com>
2022-04-09 17:40:48 -0500

    added each shape

--- js/code.js
+++ js/code.js
@@ -1,6 +1,6 @@
 //set the scene
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0xf0f0f0);
+scene.background = new THREE.Color(0x909090);
 //set the camera
 const camera = new THREE.PerspectiveCamera( 100, window.innerWidth / window.innerHeight, 3.5, 1000 );
 camera.position.set(4, 4, 4);
@@ -17,34 +17,18 @@ dirLight.position.set(10, 30, 0);
 scene.add(dirLight);
 
 
+
 //create the polycube objects
 var objects = [];
-          
-                        var mergedGeoms = []; // merge array
-                        const material = new THREE.MeshLambertMaterial( {color: 0xff0000});
 
-                        var newBox1 = new THREE.BoxBufferGeometry( 1,1,1 ); // buffer geometry
-                            newBox1.translate(0,0,0); // set position 
-                            mergedGeoms.push( newBox1 ); // add to merge array
-                        var newBox2 = new THREE.BoxBufferGeometry( 1, 1, 1 ); // 2nd buffer geometry
-                            newBox2.translate(0,0,1); // set position
-                            mergedGeoms.push( newBox2 ); // add to merge array
-                        var newBox3 = new THREE.BoxBufferGeometry( 1, 1, 1 ); // 2nd buffer geometry
-                            newBox3.translate(0,1,0); // set position
-                            mergedGeoms.push( newBox3 ); // add to merge array
-
-                        var singleGeometry = THREE.BufferGeometryUtils.mergeBufferGeometries( mergedGeoms );
-                        const object = new THREE.Mesh(singleGeometry, material); 
-                        scene.add(object);
-                        objects.push(object);
-
+//uses function from polycubes.js
+generateShapes();
 
 
 var controls = new THREE.DragControls( objects, camera, renderer.domElement );
 // add event listener to highlight dragged objects
-controls.addEventListener( 'dragstart', function ( event ) {event.object.material.emissive.set( 0x303030 );});
+controls.addEventListener( 'dragstart', function ( event ) {event.object.material.emissive.set( 0x101010 );});
 controls.addEventListener( 'dragend', function ( event ) {event.object.material.emissive.set( 0x000000 );});
-
 
 function animate() {
     requestAnimationFrame(animate);
---
+++ js/polycubes.js
@@ -0,0 +1,89 @@
+function generateShapes(){
+    var shape1 = []; // merge array
+    const material1 = new THREE.MeshLambertMaterial( {color: 0xff0000});
+    var newBox1 = new THREE.BoxBufferGeometry(1,1,3); // buffer geometry
+        newBox1.translate(0,0,0); // set position 
+        shape1.push(newBox1); // add to merge array
+    var newBox2 = new THREE.BoxBufferGeometry(1,2,1); // 2nd buffer geometry
+        newBox2.translate(-1,0.5,-1); // set position
+        shape1.push(newBox2); // add to merge array
+    var singleGeometry = THREE.BufferGeometryUtils.mergeBufferGeometries(shape1);
+    const object1 = new THREE.Mesh(singleGeometry, material1); 
+    scene.add(object1);
+    objects.push(object1);
+
+    var shape2 = []; // merge array
+    const material2 = new THREE.MeshLambertMaterial( {color: 0x00ff00});
+    var newBox1 = new THREE.BoxBufferGeometry(1,1,3); // buffer geometry
+        newBox1.translate(0,0,0); // set position 
+        shape2.push(newBox1); // add to merge array
+    var newBox2 = new THREE.BoxBufferGeometry(1,1,1); // 2nd buffer geometry
+        newBox2.translate(0,1,0); // set position
+        shape2.push(newBox2); // add to merge array
+    var singleGeometry = THREE.BufferGeometryUtils.mergeBufferGeometries(shape2);
+    const object2 = new THREE.Mesh(singleGeometry, material2); 
+    scene.add(object2);
+    objects.push(object2);
+
+    var shape3 = []; // merge array
+    const material3 = new THREE.MeshLambertMaterial( {color: 0x0000ff});
+    var newBox1 = new THREE.BoxBufferGeometry(1,3,1); // buffer geometry
+        newBox1.translate(0,0,0); // set position 
+        shape3.push(newBox1); // add to merge array
+    var newBox2 = new THREE.BoxBufferGeometry(1,1,1); // 2nd buffer geometry
+        newBox2.translate(-1,1,0); // set position
+        shape3.push(newBox2); // add to merge array
+    var singleGeometry = THREE.BufferGeometryUtils.mergeBufferGeometries(shape3);
+    const object3 = new THREE.Mesh(singleGeometry, material3); 
+    scene.add(object3);
+    objects.push(object3);
+
+    var shape4 = []; // merge array
+    const material4 = new THREE.MeshLambertMaterial( {color: 0xff00ff});
+    var newBox1 = new THREE.BoxBufferGeometry(1,2,1); // buffer geometry
+        newBox1.translate(0,0,0); // set position 
+        shape4.push(newBox1); // add to merge array
+    var newBox2 = new THREE.BoxBufferGeometry(1,2,1); // 2nd buffer geometry
+        newBox2.translate(1,1,0); // set position
+        shape4.push(newBox2); // add to merge array
+    var singleGeometry = THREE.BufferGeometryUtils.mergeBufferGeometries(shape4);
+    const object4 = new THREE.Mesh(singleGeometry, material4); 
+    scene.add(object4);
+    objects.push(object4);
+
+    var shape5 = []; // merge array
+    const material5 = new THREE.MeshLambertMaterial( {color: 0x00ffff});
+    var newBox1 = new THREE.BoxBufferGeometry(1,2,2); // buffer geometry
+        newBox1.translate(0,0,0); // set position 
+        shape5.push(newBox1); // add to merge array
+    var singleGeometry = THREE.BufferGeometryUtils.mergeBufferGeometries(shape5);
+    const object5 = new THREE.Mesh(singleGeometry, material5); 
+    scene.add(object5);
+    objects.push(object5);
+
+    var shape6 = []; // merge array
+    const material6 = new THREE.MeshLambertMaterial( {color: 0xffff00});
+    var newBox1 = new THREE.BoxBufferGeometry(1,2,1); // buffer geometry
+        newBox1.translate(0,0,0); // set position 
+        shape6.push(newBox1); // add to merge array
+    var newBox2 = new THREE.BoxBufferGeometry(1,1,1); // 2nd buffer geometry
+        newBox2.translate(1,0.5,0); // set position
+        shape6.push(newBox2); // add to merge array
+    var singleGeometry = THREE.BufferGeometryUtils.mergeBufferGeometries(shape6);
+    const object6 = new THREE.Mesh(singleGeometry, material6); 
+    scene.add(object6);
+    objects.push(object6);
+
+    var shape7 = []; //merge array
+    const material7 = new THREE.MeshLambertMaterial( {color: 0xffa500});
+    var newBox1 = new THREE.BoxBufferGeometry(1,2,1); // buffer geometry
+        newBox1.translate(0,0,0); // set position 
+        shape7.push(newBox1); // add to merge array
+    var newBox2 = new THREE.BoxBufferGeometry(1,1,1); // 2nd buffer geometry
+        newBox2.translate(1,0.5,0); // set position
+        shape7.push(newBox2); // add to merge array
+    var singleGeometry = THREE.BufferGeometryUtils.mergeBufferGeometries(shape7);
+    const object7 = new THREE.Mesh(singleGeometry, material7); 
+    scene.add(object7);
+    objects.push(object7);
+}
