e85ce694a98fef5a24a52d06102c52c7840a422a
ethanosorio <101898908+ethanosorio@users.noreply.github.com>
2022-04-09 10:49:56 -0500

    imported drag controls

--- js/code.js
+++ js/code.js
@@ -1,23 +1,47 @@
+
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+scene.background = new THREE.Color(0xf0f0f0);
+
+const camera = new THREE.PerspectiveCamera( 100, window.innerWidth / window.innerHeight, 3.5, 100 );
+camera.position.set(4, 4, 4);
+camera.lookAt(0, 0, 0);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
+scene.add(ambientLight);
+const dirLight = new THREE.DirectionalLight(0xffffff, 0.6);
+dirLight.position.set(10, 20, 0);
+scene.add(dirLight);
 
-camera.position.z = 5;
+
+
+var objects = [];
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshLambertMaterial( { color: 0xff0000 } );
+const object = new THREE.Mesh( geometry, material );
+scene.add( object );
+
+objects.push(object);
+
+var controls = new THREE.DragControls( objects, camera, renderer.domElement );
+// add event listener to highlight dragged objects
+controls.addEventListener( 'dragstart', function ( event ) {
+	event.object.material.emissive.set( 0x303030 );
+} );
+controls.addEventListener( 'dragend', function ( event ) {
+	event.object.material.emissive.set( 0x000000 );
+} );
 
 function animate() {
     requestAnimationFrame( animate );
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-
+    object.rotation.x += 0.01;
+    object.rotation.y += 0.01;
+    
     renderer.render( scene, camera );
 };
 
